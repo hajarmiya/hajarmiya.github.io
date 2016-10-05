096d907871f496596270bab05dcb9fcb9be82477
hajarmiya <naciri.chakour.hajar@gmail.com>
2016-10-05 09:17:30 -0500

    carré

--- cm215/00/sketch.js
+++ cm215/00/sketch.js
@@ -11,8 +11,9 @@ function touchStarted ()
 {
   background(255);
   textSize(50);
-  text(touches[0].x,50,50);
-    text(touches[0].y,50,100);
+  rect(touches[x],touches[y],50,50);
+  text(touches[0].x);
+  text(touches[0].y);
   
   
 }
