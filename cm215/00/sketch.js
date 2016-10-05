619ced6b332671d1b9f75bbde3ee6c5edfa9c71c
hajarmiya <naciri.chakour.hajar@gmail.com>
2016-10-05 09:50:28 -0500

    sac

--- cm215/00/sketch.js
+++ cm215/00/sketch.js
@@ -2,12 +2,13 @@ function setup() {
   createCanvas(windowWidth,windowHeight);
   rectMode(CENTER);
   stroke(0);
-       strokeWeight(3);
+  strokeWeight(3);
   
 }
 
 function draw() {
   background(255);
+  
     /**fill(random(255),random(255),random(255));*/
     
     for(var i=0;i<touches.length;i++)
@@ -16,9 +17,6 @@ function draw() {
    rect(touches[i].x,touches[i].y,100,100);
  
     }
-
-  
-  
  
 }
 function touchStarted()
