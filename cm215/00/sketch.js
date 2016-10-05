9392aa3bb4476ee6fddc94e65361fc11332a5ffe
hajarmiya <naciri.chakour.hajar@gmail.com>
2016-10-05 09:07:18 -0500

    back

--- cm215/00/sketch.js
+++ cm215/00/sketch.js
@@ -9,7 +9,10 @@ function draw() {
 }
 function touchStarted ()
 {
-  background(random(0,255),random(0,255),random(0,255));
+  background(255);
+  textSize(50);
+  text(touches[0].x,50,50);
+    text(touches[0].y,50,100);
   
   
 }
