f5fd782e1a6120fdd96b31b60d4fcff6582480f6
hajarmiya <naciri.chakour.hajar@gmail.com>
2016-10-19 09:15:40 -0500

    touches

--- cm215/00/sketch.js
+++ cm215/00/sketch.js
@@ -20,10 +20,15 @@ function draw() {
    rect(touches[i].x,touches[i].y,100,100);
  
     }
-    /*if((i++)<toucheslength){
-     /**line(touches[i].x,
-    }*/
- 
+   fill(255,0,0);
+   beginShape(); 
+     for(var i=0;i<touches.length;i++)
+     {
+       vertex(touches[i].x,touches[i].y);
+       
+     }
+     endShape(CLOSE);
+   
 }
 function touchStarted()
 {
