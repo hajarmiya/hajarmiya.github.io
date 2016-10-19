455614a9dcf99917b2f6171bb1c91c12e8b3c5e4
hajarmiya <naciri.chakour.hajar@gmail.com>
2016-10-19 11:03:55 -0500

    ball

--- rotation/r1/sketch.js
+++ rotation/r1/sketch.js
@@ -1,11 +1,22 @@
 function setup() {
  createCanvas(windowWidth,windowHeight); 
+ ellipse(CENTER);
+ balleSize=30;
+ 
 }
 
 function draw() {
+  drawBall();
+  
  background(255);
  textSize(40);
+ 
  text("Rx: " + floor( rotationX), 100, 100);
  text("Ry: " + floor (rotationY), 100, 150);
  text("Rz: " + floor( rotationZ), 100, 200);
+}
+function drawBall ()
+{
+  fill(100);
+  ellipse(windowWidth/2,windowHeight/2,balleSize,balleSize);
 }
