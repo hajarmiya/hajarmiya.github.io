425cd63b4ba8961625186adabdf0ec556f72a550
hajarmiya <naciri.chakour.hajar@gmail.com>
2016-11-02 14:51:41 -0500

    gg

--- rotation/r1/sketch.js
+++ rotation/r1/sketch.js
@@ -40,7 +40,7 @@ function draw() {
   if(dist(x,y,Px,Py)<=(balleSize/2+rayon/2))
    {x=random(0,windowWidth);
   y=random(0,windowHeight);
-    balleSize=balleSize*0.5; 
+    balleSize=balleSize*1.2; 
     
      /*if(dist(x1,y1,Px,Py)<=(balleSize/2+rayon/2))
    {Vx=-Vx;
