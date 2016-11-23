1bea8512cad1a47b2e028c7d142226d2957df099
hajarmiya <naciri.chakour.hajar@gmail.com>
2016-11-23 08:32:56 -0600

    tab

--- r1/sketch.js
+++ r1/sketch.js
@@ -1,4 +1,4 @@
-var Ax, Ay, Vy, Vx, Px, Py, Fr, score;
+var Ax, Ay, Vy, Vx, Px, Py, Fr, x1, y1, x, y, x2, y2, rayon, score;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,7 +12,14 @@ function setup() {
   score = 0;
   Vx = 0;
   Vy = 0;
-  
+  /*x = random(0, windowWidth);
+  y = random(0, windowHeight);
+  x1 = random(0, windowWidth);
+  y1 = random(0, windowHeight);
+  x2 = random(0, windowWidth);
+  y2 = random(0, windowHeight);
+  rayon = 30;
+*/
 
 }
 
@@ -58,11 +65,46 @@ function draw() {
  
   /*********/
 
+  /*fill(255, 58, 124);
+  ellipse(x, y, rayon, rayon);
+
+  fill(36, 56, 94);
+  ellipse(x1, y1, rayon, rayon);
+
+  fill(170, 1, 124);
+  ellipse(x2, y2, rayon, rayon);*/
+
+  /*******condition obstacle*******/
   
+
+  /*if (dist(x, y, Px, Py) <= (balleSize / 2 + rayon / 2)) {
+   /* x = random(0, windowWidth);
+    y = random(0, windowHeight);
+    score = score + 10;
+  } 
+  else if (dist(x2, y2, Px, Py) <= (balleSize / 2 + rayon / 2)) {
+    Vx = -Vx;
+    Vy = -Vy;
+  } 
+  else if (dist(x1, y1, Px, Py) <= (balleSize / 2 + rayon / 2)) {
+    x1 = random(0, windowWidth);
+    y2 = random(0, windowHeight);
+    score = score -10;
+  }
+  else {
+    Vx = Vx;
+    Vy = Vy;
+  }*/
+  
+
   textSize(15);
 
   text("score " + floor(score), 10, 15);
 
-  
+  /*textSize(40);
+ 
+  text("Rx: " + floor( rotationX), 100, 100);
+  text("Ry: " + floor (rotationY), 100, 150);
+  text("Rz: " + floor( rotationZ), 100, 200);*/
 }
 
