78d13cf423fd435dd174c1f3265afac12620a3ed
hajarmiya <naciri.chakour.hajar@gmail.com>
2016-12-19 14:32:11 -0600

    dmbdf

--- cm215/3/sketch.js
+++ cm215/3/sketch.js
@@ -8,17 +8,17 @@ var bN;
 var score, timer, img, imgBalle, imgObst, imgMalus;
 var obstacles = [];
 var level;
-var scoreLevel =[0,30,60];
-
+var scoreLevel =[0,30,60,80];
+/*************************************************************/
 function preload() {
-  img = loadImage("back.jpg");
+  img = loadImage("back.png");
   imgBalle = loadImage("player.png");
   imgObst = loadImage("obs.png");
   imgMalus = loadImage("malus.png");
   imgBonus = loadImage("bonus.png");
 }
 
-
+/*************************************************************/
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ellipseMode(CENTER);
@@ -36,11 +36,10 @@ function setup() {
   score = 0;
   timer = 1800;
   bN = 0.8;
-
-level = 1;
+  level = 1;
   newObstacles(level);
 }
-
+/*************************************************************/
 function draw() {
   background(img, width / 2, height / 2);
 
@@ -51,6 +50,7 @@ function draw() {
   newLevel();
 
 }
+/*************************************************************/
 function newLevel(){
   if(score >= scoreLevel[level]){
     level++;
@@ -94,7 +94,7 @@ function colision() {
     obstacles[i].display();
   }
 }
-
+/* ******************************************** */
 function drawBall() {
   image(imgBalle, pX, pY, ballSize, ballSize);
   aX = rotationY * f;
@@ -123,54 +123,57 @@ function drawBall() {
 
 
 }
-
+/* ******************************************** */
 function displayScore() {
   textSize(10);
 
-  fill(255, 0, 0);
+  fill(255);
   text("Score:" + score, windowWidth / 3, 30);
   text("level:" + level, windowWidth / 2, 30);
+  
 }
-
+/* ******************************************** */
 function displayTimer() {
-  fill(255);
-  ellipse(windowWidth / 2 + 110, 22, 30, 30);
+  /*fill(255);
+ rect(windowWidth / 2 + 110, 22, 30, 30);
   noStroke();
-  textSize(20);
-  fill(0);
-  text(int(timer / 60), width / 2 + 100, 30);
-
-
-  if (timer <= 0) {
-    obstacles[i] = false;
-
-  }
-  timer--;
-}
-
-/*function displayMessage() {
-  fill(0, 200, 255);
-  noStroke(0);
-  //strokeWeight(5);
-  rect(width / 2, height / 2, 400, 150, 10);
+  textSize(20);*/
   fill(255);
-  noStroke(0);
-  textSize(30);
-  text("GAME OVER!", width / 2, height / 2, 200, 50);
-
-  rect(width / 2, height / 2 + 40, 200, 45, 10);
-  fill(255, 125, 0);
+  text("Time:"+int(timer / 60), width / 2 + 50, 30);
+  
+  if (timer <= 0) {
+    if(score >= scoreLevel[level]){
+    level++;
+    score=0;
+    newObstacles(level);
+    timer=timer + 1800;
+    obstacles[i] = true;
+    }else{
+    gameOver();
+    }
+  
+  }else{
+    timer--;
+  }
+}
+/* ******************************************** */
+function gameOver(){
+  fill(255);
   textSize(20);
-  text("EAT AGAIN?", width / 2, height / 2 + 45, 200, 20);
-}*/
+  text("GAME OVER!", windowWidth / 2, windowHeight/2);
+}
+/* ******************************************** */
+
+
 
 function Obstacle(kind) {
   this.type = kind;
-  this.xPos = random(0, windowWidth);
-  this.yPos = random(0, windowHeight);
-  this.size = random(20, 50);
-  this.color = color(random(0, 255), random(0, 255), random(0, 255));
+   this.size = random(20, 50);
   this.radius = this.size / 2;
+  this.xPos = random(this.radius, windowWidth - this.radius);
+  this.yPos = random(this.radius, windowHeight-this.radius);
+  this.color = color(random(0, 255), random(0, 255), random(0, 255));
+  
   this.colision = false;
 this.pic;
 
